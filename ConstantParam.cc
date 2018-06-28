a2fcfb4de55b3f9af69465d1853a903ce786cf16
orsnken <hiroshima@net.itc.nagoya-u.ac.jp>
2018-06-28 16:20:37 +0900

    add edca params

--- ConstantParam.cc
+++ ConstantParam.cc
@@ -1,2 +1,26 @@
 #include "ConstantParam.h"
 
+namespace {
+
+const WLan::EdcaAc kAcVo("VO_EdcaTxopN", 0xc0);
+const WLan::EdcaAc kAcVi("VI_EdcaTxopN", 0xb8);
+const WLan::EdcaAc kAcBk("BK_EdcaTxopN", 0x28);
+const WLan::EdcaAc kAcBe("BE_EdcaTxopN", 0x70);
+
+}
+
+namespace WLan {
+
+const EdcaAc kEdcaStaAc     = kAcBk;
+const int    kEdcaStaCwMin  = 15;
+const int    kEdcaStaCwMax  = 1024;
+const int    kEdcaStaAifs   = 4;
+const int    kEdcaStaTxopMs = 0;
+
+const EdcaAc kEdcaApAc     = kAcBe;
+const int    kEdcaApCwMin  = 15;
+const int    kEdcaApCwMax  = 1024;
+const int    kEdcaApAifs   = 4;
+const int    kEdcaApTxopMs = 0;
+
+}
--- ConstantParam.h
+++ ConstantParam.h
@@ -1,9 +1,29 @@
 #ifndef CONSTANT_PARAM_H__
 #define CONSTANT_PARAM_H__
 
+#include "ns3.h"
+
+#include <string>
+
 namespace WLan {
 
+struct EdcaAc {
+  const std::string str;
+  const unsigned int id;
+  EdcaAc(std::string s, unsigned int n) : str(s), id(n) {}
+};
 
+extern const EdcaAc kEdcaStaAc;
+extern const int    kEdcaStaCwMin;
+extern const int    kEdcaStaCwMax;
+extern const int    kEdcaStaAifs;
+extern const int    kEdcaStaTxopMs;
+
+extern const EdcaAc kEdcaApAc;
+extern const int    kEdcaApCwMin;
+extern const int    kEdcaApCwMax;
+extern const int    kEdcaApAifs;
+extern const int    kEdcaApTxopMs;
 
 } // namespace WLan
 
