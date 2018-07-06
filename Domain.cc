b5e37f96164e710147c99c7c9c48f34c8faa6ced
orsnken <hiroshima@net.itc.nagoya-u.ac.jp>
2018-07-06 19:38:29 +0900

    fix parameters

--- Domain.cc
+++ Domain.cc
@@ -25,7 +25,7 @@ void Domain::Init() {
   sChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
   sChannel.AddPropagationLoss(
     "ns3::LogDistancePropagationLossModel",
-    "Exponent"         , DoubleValue(4.7),
+    "Exponent"         , DoubleValue(4.07),
     "ReferenceDistance", DoubleValue(1.0),
     "ReferenceLoss"    , DoubleValue(40.045997)
   );
@@ -41,12 +41,10 @@ void Domain::Init() {
   sPhy = YansWifiPhyHelper::Default();
   sPhy.SetPcapDataLinkType(YansWifiPhyHelper::DLT_IEEE802_11_RADIO);
   sPhy.SetChannel(sChannel.Create());
-  /*
   sPhy.Set("EnergyDetectionThreshold", DoubleValue(-96));
   sPhy.Set("CcaMode1Threshold"       , DoubleValue(-99));
   sPhy.Set("TxPowerEnd"              , DoubleValue(10.0206));
   sPhy.Set("TxPowerStart"            , DoubleValue(10.0206));
-  */
 }
 
 // ----------------------------------------------------------------
