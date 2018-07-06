d9e1f8ff569c0e022bb9ae1cfe0674312e818868
orsnken <hiroshima@net.itc.nagoya-u.ac.jp>
2018-07-06 18:30:24 +0900

    add global parameters

---
+++ SimulationSettings.cc
@@ -0,0 +1,12 @@
+#include "ConstantParam.h"
+#include "SimulationSettings.h"
+
+namespace WLan {
+
+namespace Global {
+
+double distanceWLans = kDistanceWLans;
+
+} // namespace Global
+
+} // namespace WLan
---
+++ SimulationSettings.h
@@ -0,0 +1,14 @@
+#ifndef SIMULATION_SETTINGS_H__
+#define SIMULATION_SETTINGS_H__
+
+namespace WLan {
+
+namespace Global {
+
+extern double distanceWLans;
+
+} // namespace Global
+
+} // namespace WLan
+
+#endif  // SIMULATION_SETTINGS_H__
